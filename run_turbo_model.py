a3f4a43010b9c05ab53b1b0eaafef9848e5e7304
alexkors <alex.kors.lg@gmail.com>
2023-03-12 20:58:11 +0200

    add separate chats for users

--- run_turbo_model.py
+++ run_turbo_model.py
@@ -11,25 +11,30 @@ openai.api_key = os.getenv('OPENAI_PASSWORD')
 bot = Bot(os.getenv('TELEGRAM_BOT_TOKEN'))
 dp = Dispatcher(bot)
 
-ai_messages = [
-    {'role': 'system', 'content': 'You are a helpful assistant.'},
-    {'role': 'user', 'content': 'Do you know everething?'},
-    {'role': 'assistant',
-        'content': 'As an AI language model, I have knowledge on a wide range of topics'}
-]
+BOT_ROLE = 'You are a helpful assistant.'
+bot_chats = {}
 
 
-def update(messages, role, content):
-    messages.append({'role': role, 'content': content})
-    return messages
+def update(bot_chat, role, content):
+    bot_chat.append({'role': role, 'content': content})
+    return bot_chat
 
 
 @dp.message_handler()
-async def send(message: types.Message):
-    update(ai_messages, 'user', message.text)
+async def chat_handler(message: types.Message):
+    await bot.send_chat_action(message.chat.id, action=types.ChatActions.TYPING)
+
+    user_id = message.from_user.id
+    user_name = message.from_user.full_name
+
+    if user_id not in bot_chats:
+        bot_chats[user_id] = [{'role': 'system', 'content': BOT_ROLE}]
+        update(bot_chats[user_id], 'user', f'Hi, my name is {user_name}')
+    update(bot_chats[user_id], 'user', message.text)
+
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=ai_messages
+        messages=bot_chats[user_id]
     )
 
     await message.answer(response['choices'][0]['message']['content'])
